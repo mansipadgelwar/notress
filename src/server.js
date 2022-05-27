52d76aa0070fee480501eb5d58ef62aed04396ab
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-27 13:58:39 +0530

    Initialize project using Create React App

---
+++ src/server.js
@@ -0,0 +1,86 @@
+import { Server, Model, RestSerializer } from "miragejs";
+import {
+  deleteFromArchivesHandler,
+  getAllArchivedNotesHandler,
+  restoreFromArchivesHandler,
+} from "./backend/controllers/ArchiveController";
+import {
+  loginHandler,
+  signupHandler,
+  userProfilehandler,
+} from "./backend/controllers/AuthController";
+import {
+  archiveNoteHandler,
+  createNoteHandler,
+  deleteNoteHandler,
+  getAllNotesHandler,
+  trashNoteHandler,
+  updateNoteHandler,
+} from "./backend/controllers/NotesController";
+import {
+  deleteFromTrashHandler,
+  getAllTrashNotesHandler,
+  restoreFromTrashHandler,
+} from "./backend/controllers/TrashController";
+import { users } from "./backend/db/users";
+
+export function makeServer({ environment = "development" } = {}) {
+  const server = new Server({
+    serializers: {
+      application: RestSerializer,
+    },
+    environment,
+    // TODO: Use Relationships to have named relational Data
+    models: {
+      user: Model,
+      notes: Model,
+    },
+
+    seeds(server) {
+      server.logging = false;
+      users.forEach((item) =>
+        server.create("user", {
+          ...item,
+          notes: [],
+          archives: [],
+          trash: [],
+        })
+      );
+    },
+
+    routes() {
+      this.namespace = "api";
+
+      // auth routes (public)
+      this.post("/auth/signup", signupHandler.bind(this));
+      this.post("/auth/login", loginHandler.bind(this));
+
+      // user route (private)
+      this.get("/user", userProfilehandler.bind(this));
+
+      // notes routes (private)
+      this.get("/notes", getAllNotesHandler.bind(this));
+      this.post("/notes", createNoteHandler.bind(this));
+      this.post("/notes/:noteId", updateNoteHandler.bind(this));
+      this.delete("/notes/:noteId", deleteNoteHandler.bind(this));
+      this.post("/notes/archives/:noteId", archiveNoteHandler.bind(this));
+      this.post("/notes/trash/:noteId", trashNoteHandler.bind(this));
+
+      // archive routes (private)
+      this.get("/archives", getAllArchivedNotesHandler.bind(this));
+      this.post(
+        "/archives/restore/:noteId",
+        restoreFromArchivesHandler.bind(this)
+      );
+      this.delete(
+        "/archives/delete/:noteId",
+        deleteFromArchivesHandler.bind(this)
+      );
+      // trash routes (private)
+      this.get("/trash", getAllTrashNotesHandler.bind(this));
+      this.post("/trash/restore/:noteId", restoreFromTrashHandler.bind(this));
+      this.delete("/trash/delete/:noteId", deleteFromTrashHandler.bind(this));
+    },
+  });
+  return server;
+}
