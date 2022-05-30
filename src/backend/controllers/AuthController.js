85c3ae26203dc697824aafcc2574ca28a424b3e6
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-05-30 13:03:28 +0530

    Update AuthController.js

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -99,16 +99,16 @@ export const loginHandler = function (schema, request) {
   }
 };
 
-export const userProfilehandler = function (schema, request) {
-  const user = requiresAuth.call(this, request);
-  if (!user) {
-    return new Response(
-      404,
-      {},
-      {
-        errors: ["The email you entered is not Registered. Not Found error"],
-      }
-    );
-  }
-  return new Response(200, {}, { user });
-};
+// export const userProfilehandler = function (schema, request) {
+//   const user = requiresAuth.call(this, request);
+//   if (!user) {
+//     return new Response(
+//       404,
+//       {},
+//       {
+//         errors: ["The email you entered is not Registered. Not Found error"],
+//       }
+//     );
+//   }
+//   return new Response(200, {}, { user });
+// };
