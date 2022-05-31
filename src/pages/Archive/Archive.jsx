f7f44cdd9086cf526bcf07ff23641eb8348b09e9
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-31 21:54:04 +0530

    feat: add archive page

--- src/pages/Archive/Archive.jsx
+++ src/pages/Archive/Archive.jsx
@@ -1,5 +1,4 @@
 import { NotesMenuBar, SearchBar, SideBar } from "../../components";
-import WysiwygEditor from "../../components/editor/WysiwygEditor";
 import "../../pages/pages.css";
 
 const Archive = () => {
@@ -11,25 +10,41 @@ const Archive = () => {
 
       <div className="main-content-page">
         <div className="hero-img">
-          <div className="hero-img-overlay">
+          <div>
             <SearchBar />
-            <div className="notes-container">
-              <div className="notes-title-container">
-                <div className="h4 text-bold">Title of the note</div>
-                <div>
-                  <span className="material-icons">push_pin</span>
-                </div>
+          </div>
+
+          <div className="section-breaker h4 text-bold">
+            <div>ARCHIVED</div>
+          </div>
+
+          <div className="notes-container">
+            <div className="notes-title-container">
+              <div className="h4 text-bold">Title of the note</div>
+              <div>
+                <span className="material-icons">push_pin</span>
               </div>
-              <div className="notes-body">
-                <WysiwygEditor />
-                If body of the note is longer than the size of the card then the
-                card will increase its height
+            </div>
+            <div className="notes-body"></div>
+            <div className="notes-label-type text-bold h5">LABEL 1</div>
+            <div className="notes-menu">
+              <div className="notes-creation-date">Created on 26/10/2021</div>
+              <NotesMenuBar />
+            </div>
+          </div>
+
+          <div className="notes-container">
+            <div className="notes-title-container">
+              <div className="h4 text-bold">Title of the note</div>
+              <div>
+                <span className="material-icons">push_pin</span>
               </div>
-              <div className="notes-label-type text-bold h5">LABEL 1</div>
-              <div className="notes-menu">
-                <div className="notes-creation-date">Created on 26/10/2021</div>
-                <NotesMenuBar />
-              </div>
+            </div>
+            <div className="notes-body"></div>
+            <div className="notes-label-type text-bold h5">LABEL 1</div>
+            <div className="notes-menu">
+              <div className="notes-creation-date">Created on 26/10/2021</div>
+              <NotesMenuBar />
             </div>
           </div>
         </div>
