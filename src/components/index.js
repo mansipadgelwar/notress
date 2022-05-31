f833024b889f6481d30df33759ef4c27824bc00a
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-31 22:21:19 +0530

    export filter modal

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { NavBar } from "./Navbar/NavBar";
 export { NotesMenuBar } from "./NotesMenuBar/NotesMenuBar";
 export { SearchBar } from "./SearchBar/SearchBar";
 export { SideBar } from "./SideBar/SideBar";
+export { FilterModal } from "../components/Modals/FilterModal/Filtermodal.jsx";
