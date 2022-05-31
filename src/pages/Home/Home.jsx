f9dc006214347a5672858f5ff3fa8681a4bcdc47
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-31 16:17:17 +0530

    style: navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,8 +10,56 @@ const Home = () => {
 
       <div className="main-content-page">
         <div className="hero-img">
-          <div className="search-bar-container">
+          <div>
             <SearchBar />
+          </div>
+          <div className="notes-container">
+            <div className="notes-title-container">
+              <div className="h4 text-bold">Title of the note</div>
+              <div>
+                <span className="material-icons">push_pin</span>
+              </div>
+            </div>
+            <div className="notes-body">
+              <WysiwygEditor />
+            </div>
+            <div className="notes-label-type text-bold h5">LABEL 1</div>
+            <div className="notes-menu">
+              <div className="notes-creation-date">Created on 26/10/2021</div>
+              <NotesMenuBar />
+            </div>
+          </div>
+          <div className="notes-container">
+            <div className="notes-title-container">
+              <div className="h4 text-bold">Title of the note</div>
+              <div>
+                <span className="material-icons">push_pin</span>
+              </div>
+            </div>
+            <div className="notes-body">
+              <WysiwygEditor />
+            </div>
+            <div className="notes-label-type text-bold h5">LABEL 1</div>
+            <div className="notes-menu">
+              <div className="notes-creation-date">Created on 26/10/2021</div>
+              <NotesMenuBar />
+            </div>
+          </div>
+          <div className="notes-container">
+            <div className="notes-title-container">
+              <div className="h4 text-bold">Title of the note</div>
+              <div>
+                <span className="material-icons">push_pin</span>
+              </div>
+            </div>
+            <div className="notes-body">
+              <WysiwygEditor />
+            </div>
+            <div className="notes-label-type text-bold h5">LABEL 1</div>
+            <div className="notes-menu">
+              <div className="notes-creation-date">Created on 26/10/2021</div>
+              <NotesMenuBar />
+            </div>
           </div>
           <div className="notes-container">
             <div className="notes-title-container">
