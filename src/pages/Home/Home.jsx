be33ee3a4ae68e662719c0d14477eb9eb001dcf3
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-31 19:06:46 +0530

    refcator code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -29,6 +29,7 @@ const Home = () => {
               <NotesMenuBar />
             </div>
           </div>
+
           <div className="notes-container">
             <div className="notes-title-container">
               <div className="h4 text-bold">Title of the note</div>
@@ -36,41 +37,7 @@ const Home = () => {
                 <span className="material-icons">push_pin</span>
               </div>
             </div>
-            <div className="notes-body">
-              <WysiwygEditor />
-            </div>
-            <div className="notes-label-type text-bold h5">LABEL 1</div>
-            <div className="notes-menu">
-              <div className="notes-creation-date">Created on 26/10/2021</div>
-              <NotesMenuBar />
-            </div>
-          </div>
-          <div className="notes-container">
-            <div className="notes-title-container">
-              <div className="h4 text-bold">Title of the note</div>
-              <div>
-                <span className="material-icons">push_pin</span>
-              </div>
-            </div>
-            <div className="notes-body">
-              <WysiwygEditor />
-            </div>
-            <div className="notes-label-type text-bold h5">LABEL 1</div>
-            <div className="notes-menu">
-              <div className="notes-creation-date">Created on 26/10/2021</div>
-              <NotesMenuBar />
-            </div>
-          </div>
-          <div className="notes-container">
-            <div className="notes-title-container">
-              <div className="h4 text-bold">Title of the note</div>
-              <div>
-                <span className="material-icons">push_pin</span>
-              </div>
-            </div>
-            <div className="notes-body">
-              <WysiwygEditor />
-            </div>
+            <div className="notes-body"></div>
             <div className="notes-label-type text-bold h5">LABEL 1</div>
             <div className="notes-menu">
               <div className="notes-creation-date">Created on 26/10/2021</div>
