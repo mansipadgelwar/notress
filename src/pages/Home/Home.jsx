7798d7a9ddf0cc5fa073a6ea5b3afca0bb0a4114
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-06-01 17:12:44 +0530

    style quill editor

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { NotesMenuBar, SearchBar, SideBar } from "../../components";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Home = () => {
   return (
@@ -13,14 +15,18 @@ const Home = () => {
             <SearchBar />
           </div>
           <div className="notes-container">
-            <div className="notes-title-container">
-              <div className="h4 text-bold">Title of the note</div>
-              <div>
-                <span className="material-icons">push_pin</span>
+            <div className="notes-editor-conatiner">
+              <div className="notes-title-container">
+                <div className="h4 text-bold">Title of the note</div>
+                <div>
+                  <span className="material-icons">push_pin</span>
+                </div>
               </div>
+              <div className="notes-body">
+                <ReactQuill />
+              </div>
+              <div className="notes-label-type text-bold h5">LABEL 1</div>
             </div>
-            <div className="notes-body"></div>
-            <div className="notes-label-type text-bold h5">LABEL 1</div>
             <div className="notes-menu">
               <div className="notes-creation-date">Created on 26/10/2021</div>
               <NotesMenuBar />
