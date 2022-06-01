0df1fab43737661ff91300d113c8d484b4ba057f
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-06-01 07:47:54 +0530

    feat: add authprovider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./context/authContext/authenticationContext";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
