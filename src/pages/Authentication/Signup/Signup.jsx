b92d929a6c9018ab58c23aa4a751c742adf99f70
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-06-06 10:21:46 +0530

    fix: context api path

--- src/pages/Authentication/Signup/Signup.jsx
+++ src/pages/Authentication/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context";
+import { useAuth } from "../../../context";
 
 const Signup = () => {
   const initialFormDetails = {
