b99ebf236f737b96ab2da890806c84d7a061c3b2
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-06-08 09:12:51 +0530

    fix: editor and notes position

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -32,97 +32,97 @@ const Home = () => {
             showEditorModal={showEditorModal}
             onClosingEditorModal={() => setShowEditorModal(false)}
           />
-        </div>
 
-        {showFilterData
-          ? filterState.filteredData.map((item) => {
-              return (
-                <div
-                  className="notes-container"
-                  key={item._id}
-                  style={{ ...item, backgroundColor: item.bgColor }}
-                >
-                  <div className="notes-title-container">
-                    <div className="h4 text-bold">{item.title}</div>
-                    <div>
-                      <span
-                        className={
-                          item.pinnedNote
-                            ? "material-icons"
-                            : "material-icons-outlined"
-                        }
-                      >
-                        push_pin
-                      </span>
-                    </div>
-                  </div>
+          {showFilterData
+            ? filterState.filteredData.map((item) => {
+                return (
                   <div
-                    className="notes-body"
-                    dangerouslySetInnerHTML={{ __html: item.body }}
-                  />
-                  <div class="notes-label-container">
-                    {item.tags.map((label) => {
-                      return (
-                        <div className="notes-label-type text-bold h5">
-                          {label}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div className="notes-menu">
-                    <div className="notes-creation-date">
-                      {`${new Date(item.createdTime).toLocaleString()}`}
+                    className="notes-container"
+                    key={item._id}
+                    style={{ ...item, backgroundColor: item.bgColor }}
+                  >
+                    <div className="notes-title-container">
+                      <div className="h4 text-bold">{item.title}</div>
+                      <div>
+                        <span
+                          className={
+                            item.pinnedNote
+                              ? "material-icons"
+                              : "material-icons-outlined"
+                          }
+                        >
+                          push_pin
+                        </span>
+                      </div>
                     </div>
-                    <div className="priority-tab">{item.priority}</div>
-                    <NotesMenuBar notes={item} menutype={false} />
-                  </div>
-                </div>
-              );
-            })
-          : state.notes.map((item) => {
-              return (
-                <div
-                  className="notes-container"
-                  key={item._id}
-                  style={{ ...item, backgroundColor: item.bgColor }}
-                >
-                  <div className="notes-title-container">
-                    <div className="h4 text-bold">{item.title}</div>
-                    <div>
-                      <span
-                        className={
-                          item.pinnedNote
-                            ? "material-icons"
-                            : "material-icons-outlined"
-                        }
-                      >
-                        push_pin
-                      </span>
+                    <div
+                      className="notes-body"
+                      dangerouslySetInnerHTML={{ __html: item.body }}
+                    />
+                    <div class="notes-label-container">
+                      {item.tags.map((label) => {
+                        return (
+                          <div className="notes-label-type text-bold h5">
+                            {label}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className="notes-menu">
+                      <div className="notes-creation-date">
+                        {`${new Date(item.createdTime).toLocaleString()}`}
+                      </div>
+                      <div className="priority-tab">{item.priority}</div>
+                      <NotesMenuBar notes={item} menutype={false} />
                     </div>
                   </div>
+                );
+              })
+            : state.notes.map((item) => {
+                return (
                   <div
-                    className="notes-body"
-                    dangerouslySetInnerHTML={{ __html: item.body }}
-                  />
-                  <div class="notes-label-container">
-                    {item.tags.map((label) => {
-                      return (
-                        <div className="notes-label-type text-bold h5">
-                          {label}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div className="notes-menu">
-                    <div className="notes-creation-date">
-                      {`${new Date(item.createdTime).toLocaleString()}`}
+                    className="notes-container"
+                    key={item._id}
+                    style={{ ...item, backgroundColor: item.bgColor }}
+                  >
+                    <div className="notes-title-container">
+                      <div className="h4 text-bold">{item.title}</div>
+                      <div>
+                        <span
+                          className={
+                            item.pinnedNote
+                              ? "material-icons"
+                              : "material-icons-outlined"
+                          }
+                        >
+                          push_pin
+                        </span>
+                      </div>
                     </div>
-                    <div className="priority-tab">{item.priority}</div>
-                    <NotesMenuBar notes={item} menutype={false} />
+                    <div
+                      className="notes-body"
+                      dangerouslySetInnerHTML={{ __html: item.body }}
+                    />
+                    <div class="notes-label-container">
+                      {item.tags.map((label) => {
+                        return (
+                          <div className="notes-label-type text-bold h5">
+                            {label}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className="notes-menu">
+                      <div className="notes-creation-date">
+                        {`${new Date(item.createdTime).toLocaleString()}`}
+                      </div>
+                      <div className="priority-tab">{item.priority}</div>
+                      <NotesMenuBar notes={item} menutype={false} />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+        </div>
       </div>
     </div>
   );
